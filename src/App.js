538e95dd1a14c2ebbc1532eedec8848430225b6f
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-14 12:10:44 -0700

    mapped over the data to get the Post element in PostsPage

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
 // import the PostsPage and SearchBar and add them to the App
+import PostsPage from './components/PostsContainer/PostsPage'
+import SearchBar from './components/SearchBar/SearchBarContainer'
+import DummyData from './dummy-data'
+import "./App.css";
+
+
 
 
 const App = () => {
-
+  const [posts, setPosts] = useState(DummyData)
   return (
     <div className="App">
       {/* Add imported components here to render them */}
-      
+      <PostsPage posts={posts} />
     </div>
   );
 };
